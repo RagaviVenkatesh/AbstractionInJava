d935bbb9cf06be24c051cb3bf438a92f5a3fd88b
Ragavi <49680219+RagaviVenkatesh@users.noreply.github.com>
2025-06-28 13:33:34 +0530

    Create MobileNetflixPlayer.java

---
+++ MobileNetflixPlayer.java
@@ -0,0 +1,6 @@
+public class MobileNetflixPlayer extends NetflixPlayer {
+    @Override
+    public void play(String title) {
+        System.out.println("Streaming '" + title + "' on Mobile 📱");
+    }
+}
